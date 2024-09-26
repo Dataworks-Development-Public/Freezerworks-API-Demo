75370493a54ba83ef7404fc851b4f5fe8525aa05
Keaton Robinson <krobinson@dwdev.com>
2024-09-25 17:59:19 -0700

    removed polling

--- fw-shopping-browser/src/app/app.component.ts
+++ fw-shopping-browser/src/app/app.component.ts
@@ -24,6 +24,6 @@ export class AppComponent implements OnInit {
   ){ }  
 
   ngOnInit(): void {
-    this.aliquotSvc.syncAvailAliquotGroups();
+    this.aliquotSvc.refreshAvailAliquotGroups();
   }
 }
